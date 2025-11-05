014bb1c0872452e109dc7ae4516e3e2f826cca6a
yasina08 <yasinayasi20@gmail.com>
2025-11-05 15:02:54 +0530

    Add files via upload

---
+++ biggest.py
@@ -0,0 +1,9 @@
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+c=float(input("enter third number:"))
+if a>b and a>c:
+    print("the biggest number is:",a)
+elif b>a and b>c:
+    print("the biggest number is:",b)
+else:
+    print("the biggest number is:",c)    
